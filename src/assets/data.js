26582d56f5c9bb72b6f8d830ef78036f5e26a4d0
Aless4ndr0 <jiwemoyo@gmail.com>
2024-04-16 12:53:22 -0500

    Portfolio in Astro

---
+++ src/assets/data.js
@@ -0,0 +1,15 @@
+export const socials = [
+  {
+    logo: "bx bxl-github",
+  },
+
+  {
+    logo: "bx bxl-linkedin-square",
+  },
+  {
+    logo: "bx bxl-instagram",
+  },
+  
+];
+
+export const info=[{text:'About'},{text:'Projects'}]
