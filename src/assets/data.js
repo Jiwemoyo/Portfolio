d4a43e9b589919ac3a50d832bae141513aa325fb
Aless4ndr0 <jiwemoyo@gmail.com>
2024-04-16 13:17:08 -0500

    Empezando a crear la data de los iconos

--- src/assets/data.js
+++ src/assets/data.js
@@ -9,7 +9,33 @@ export const socials = [
   {
     logo: "bx bxl-instagram",
   },
-  
 ];
 
-export const info=[{text:'About'},{text:'Projects'}]
+export const info = [{ text: "About" }, { text: "Projects" }];
+
+export const technologies = [
+  {
+    text: "TypeScript",
+    logo: "bx bxl-typescript",
+  },
+  {
+    text: "JavaScript",
+    logo: "bx bxl-javascript",
+  },
+  {
+    text: "PostgresSQL",
+    logo: "bx bxl-postgresql",
+  },
+  {
+    text: "Docker",
+    logo: "bx bxl-docker",
+  },
+  {
+    text: "MongoDb",
+    logo: "bx bxl-mongodb",
+  },
+  {
+    text: "Tailwind",
+    logo: "bx bxl-tailwind-css",
+  },
+];
