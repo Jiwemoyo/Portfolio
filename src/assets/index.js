35ced7ff18fe591a7b39dbbfd8299d9a911b8cbc
Aless4ndr0 <jiwemoyo@gmail.com>
2024-04-18 22:22:31 -0500

    agregando
    Agregue documentos

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,34 @@
-document.addEventListener('astro:page-load',()=>{
-    const wrapper=document.querySelector('[data-tech-wrapper]')
-    let copy=document.querySelector('.tech-card')?.cloneNode(true)
-    wrapper.append(copy)
-})
+document.addEventListener("astro:page-load", () => {
+  /*============================= Slider ==================================*/
+  const wrapper = document.querySelector("[data-tech-wrapper]");
+  let copy = document.querySelector(".tech-card")?.cloneNode(true);
+  wrapper.append(copy);
+  /*============================= Modal ==================================*/
+  const modal = document.querySelector("[data-tech-modal]");
+  const h1 = document.querySelector("[data-tech-h1]");
+  const stop = document.querySelector("[data-tech-stop]");
+
+  h1.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+    modal.classList.add("flex");
+  });
+
+  wrapper.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+    modal.classList.add("flex");
+  });
+
+  modal.addEventListener("click", () => {
+    modal.classList.remove("flex");
+    modal.classList.toggle("hidden");
+  });
+
+  stop.addEventListener("click", (e) => e.stopPropagation());
+
+  addEventListener('keydown',(e)=>{
+    if (e.key === 'Escape') {
+        modal.classList.remove('flex')
+        modal.classList.add('hidden')
+    }
+  })
+});
